4e7be041281e53e5351f1e89ff526d7c500a501e
7rans <transfire@gmail.com>
2011-11-10 18:45:19 -0500

    Support REE and JRuby.

--- lib/cli/core_ext.rb
+++ lib/cli/core_ext.rb
@@ -20,48 +20,6 @@ class UnboundMethod
       def source_location
         to_java.source_location(Thread.current.to_java.getContext())
       end
-
-    else
-
-      # Return the source location of an instance method for Ruby 1.8.
-      # @return [Array] A two element array. First element is the
-      #   file, second element is the line in the file where the
-      #   method definition is found.
-      def source_location
-        klass = case owner
-                when Class
-                  owner
-                when Module
-                  method_owner = owner
-                  Class.new { include(method_owner) }
-                end
-
-        # deal with immediate values
-        case
-        when klass == Symbol
-          return :a.method(name).source_location
-        when klass == Fixnum
-          return 0.method(name).source_location
-        when klass == TrueClass
-          return true.method(name).source_location
-        when klass == FalseClass
-          return false.method(name).source_location
-        when klass == NilClass
-          return nil.method(name).source_location
-        end
-
-        begin
-          klass.allocate.method(name).source_location
-        rescue TypeError
-
-          # Assume we are dealing with a Singleton Class:
-          # 1. Get the instance object
-          # 2. Forward the source_location lookup to the instance
-          instance ||= ObjectSpace.each_object(owner).first
-          instance.method(name).source_location
-        end
-      end
-
     end
 
   end
