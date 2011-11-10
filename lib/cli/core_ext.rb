fc9836ada8964903c6037df1a9699273e2cf937c
7rans <transfire@gmail.com>
2011-11-10 18:51:21 -0500

    Just bare bones core_ext.

--- lib/cli/core_ext.rb
+++ lib/cli/core_ext.rb
@@ -1,27 +1,15 @@
-# This code is a derivation of Dmitry Elastic work
-# in the method_source gem.
-
 class UnboundMethod
-
   if !method_defined?(:source_location)
-
-    if Proc.method_defined? :__file__
-      # Ruby enterprise edition provides all the information that's
-      # needed, in a slightly different way.
+    if Proc.method_defined? :__file__  # /ree/
       def source_location
         [__file__, __line__] rescue nil
       end
-
     elsif defined?(RUBY_ENGINE) && RUBY_ENGINE =~ /jruby/
       require 'java'
-
-      # JRuby version source_location hack
-      # @return [Array] A two element array containing the source location of the method
       def source_location
         to_java.source_location(Thread.current.to_java.getContext())
       end
     end
-
   end
-
 end
+
